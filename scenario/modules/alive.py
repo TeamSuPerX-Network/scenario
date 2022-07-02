cb1e1887b832f933a82aa842409811364e4894f7
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-07-03 00:47:22 +0530

    Update alive.py

--- scenario/modules/alive.py
+++ scenario/modules/alive.py
@@ -33,7 +33,7 @@ async def awake(event):
   await tbot.send_file(event.chat_id, ran, caption=TEXT,  buttons=BUTTON)
 
 __help__ = """
-/epo - Get repo
+/repo - Get repo
 /alive - Alive status
 """
 
